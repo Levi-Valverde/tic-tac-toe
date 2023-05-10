aa676d4b874f11cb91f4a133dad21a340452c64a
levi <levivalverdelima@gmail.com>
2023-05-10 07:10:27 -0300

    changing the files

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const gameBoard = document.getElementById("gameboard");
 const infoDisplay = document.getElementById("info");
 const startCells = ["", "", "", "", "", "", "", "", ""];
 
+let go;
+
 function createBoard() {
   startCells.forEach((cell, index) => {
     const cellElement = document.createElement("div");
     cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener("click", addGo);
     gameBoard.appendChild(cellElement);
   });
 }
 
 createBoard();
+
+function addGo(e) {
+  const goDisplay = document.createElement("div");
+  goDisplay.classList.add(go);
+  e.target.appendChild(goDisplay);
+  go = go === "circle" ? "cross" : "circle";
+  infoDisplay.textContent = `it is now "${go}"'s go.`;
+  e.target.removeEventListener("click", addGo);
+}
